2246eb7b1728dba802176beff10809af40ea0900
Abdiel Martinez <aabdiel1998@gmail.com>
2025-01-20 21:35:11 -0600

    ✨ added Winston logger for improved logging capabilities

---
+++ src/services/logger.service.ts
@@ -0,0 +1,24 @@
+import { createLogger, format, transports, config } from "winston";
+
+const myFormat = format.combine(
+  format.timestamp(),
+  format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
+);
+
+const loggerInstance = createLogger({
+  levels: config.syslog.levels,
+  format: myFormat,
+  transports: [new transports.Console()],
+});
+
+export const loggerLevels = {
+  error: "error",
+  warn: "warn",
+  info: "info",
+  http: "http",
+  verbose: "verbose",
+  debug: "debug",
+  silly: "silly",
+} as const;
+
+export default loggerInstance;
