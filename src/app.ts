297220bca6588065920d7531c44c9682a1866f50
Abdiel Martinez <aabdiel1998@gmail.com>
2025-01-21 23:09:49 -0600

    ✨ added authentication and contact routes, refactored message handling with MessageService, and improved middleware setup

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import "reflect-metadata";
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -6,30 +7,58 @@ import bodyParser from "koa-bodyparser";
 import cors from "@koa/cors";
 import { createServer } from "http";
 import { createMessageRouter } from "./routes/message.routes";
+import { createContactRouter } from "./routes/contact.routes";
+import { createAuthRouter } from "./routes/auth.routes";
 import { SocketService } from "./services/socket.service";
 import { WhatsappService } from "./services/whatsapp.service";
-import { MessageScheduler } from "./scheduler/message.scheduler";
 import { mainRouter } from "./routes/main.routes";
 import logger, { loggerLevels } from "./services/logger.service";
+import { ContactService } from "./services/contact.service";
+import { AppDataSource } from "./database";
+import { Contact } from "./entity/contact";
+import { Message } from "./entity/message";
+import { MessageService } from "./services/messages.service";
 
 async function startServer() {
   const app = new Koa();
   const server = createServer(app.callback());
 
-  const socketService = new SocketService(server);
-  const whatsappService = new WhatsappService(socketService);
-  const messageScheduler = new MessageScheduler(whatsappService);
-  whatsappService.init();
-
   // Middleware
   app.use(cors());
   app.use(bodyParser());
+
+  await AppDataSource.initialize()
+    .then(() => logger.info(loggerLevels.info, "Database initialized"))
+    .catch((err) => logger.error(err));
+
+  const socketService = new SocketService(server);
+  const whatsappService = new WhatsappService(socketService);
+  whatsappService.init();
+
+  // Contact initialization
+  const contactRepository = AppDataSource.getRepository(Contact);
+  const contactService = new ContactService(contactRepository, whatsappService);
+  const contactRouter = createContactRouter(contactService);
+  app.use(contactRouter.routes());
+  app.use(contactRouter.allowedMethods());
+
+  // Auth initialization
+  const authRouter = createAuthRouter(whatsappService);
+  app.use(authRouter.routes());
+  app.use(authRouter.allowedMethods());
 
   // Routes
   app.use(mainRouter.routes());
   app.use(mainRouter.allowedMethods());
 
-  const messageRouter = createMessageRouter(whatsappService, messageScheduler);
+  // Message initialization
+  const messageRepository = AppDataSource.getRepository(Message);
+  const messageService = new MessageService(
+    messageRepository,
+    contactService,
+    whatsappService
+  );
+  const messageRouter = createMessageRouter(messageService);
   app.use(messageRouter.routes());
   app.use(messageRouter.allowedMethods());
 
