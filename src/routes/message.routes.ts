20fd8fc665d76a639b46cfece6322a12d7e3d6f7
Abdiel Martinez <aabdiel1998@gmail.com>
2025-01-21 23:09:32 -0600

    ♻️ refactor message routes and implement MessageService for improved message handling and scheduling

--- src/routes/message.routes.ts
+++ src/routes/message.routes.ts
@@ -1,34 +1,22 @@
 import Router from "@koa/router";
 import { Context } from "koa";
 
-import { MessageScheduler } from "../scheduler/message.scheduler";
-import { WhatsappService } from "../services/whatsapp.service";
 import { SendMessageSchema, sendMessageSchema } from "../dto/message.dto";
 import logger from "../services/logger.service";
+import { MessageService } from "../services/messages.service";
 
-export const createMessageRouter = (
-  whatsAppService: WhatsappService,
-  messageScheduler: MessageScheduler
-) => {
+export const createMessageRouter = (messageService: MessageService) => {
   const router = new Router({
-    prefix: "/api",
+    prefix: "/api/messages",
   });
 
-  router.post("/messages", async (ctx: Context) => {
+  router.post("/", async (ctx: Context) => {
     try {
       const formData = ctx.request.body as SendMessageSchema;
-      const { contacts, message, datetime } = sendMessageSchema.parse(formData);
+      const data = sendMessageSchema.parse(formData);
+      const result = await messageService.handleMessage(data);
 
-      const date = datetime ? new Date(datetime) : undefined;
-
-      if (date && date > new Date()) {
-        messageScheduler.scheduleMessage(contacts, message, date);
-        ctx.body = { message: "Message scheduled" };
-        return;
-      }
-
-      const result = await whatsAppService.sendMessage(contacts, message);
-      ctx.body = { message: "Message sent", result };
+      ctx.body = { result };
     } catch (error) {
       logger.error(error);
       ctx.status = 400;
@@ -36,20 +24,13 @@ export const createMessageRouter = (
     }
   });
 
-  router.post("/logout", async (ctx: Context) => {
-    await whatsAppService.logout();
-    ctx.body = { message: "Logged out" };
-  });
-
-  router.get("/contacts", async (ctx: Context) => {
+  router.get("/", async (ctx: Context) => {
     try {
-      const contacts = await whatsAppService.getContacts();
-
-      ctx.body = { contacts };
+      const messages = await messageService.getMessages();
+      ctx.body = { messages };
     } catch (error) {
       logger.error(error);
       ctx.status = 400;
-      ctx.body = { error };
     }
   });
 
---
+++ src/services/messages.service.ts
@@ -0,0 +1,102 @@
+import { SendMessageSchema } from "../dto/message.dto";
+import schedule from "node-schedule";
+import { Message, Status } from "../entity/message";
+import { Repository } from "typeorm";
+import { WhatsappService } from "./whatsapp.service";
+import { ContactService } from "./contact.service";
+import logger, { loggerLevels } from "../services/logger.service";
+import WAWebJS from "whatsapp-web.js";
+
+export class MessageService {
+  constructor(
+    private readonly messageRepository: Repository<Message>,
+    private readonly contactService: ContactService,
+    private readonly whatsappService: WhatsappService
+  ) {
+    this.syncPendingMessages();
+  }
+
+  public async getMessages(): Promise<Message[]> {
+    return this.messageRepository.find();
+  }
+
+  public async handleMessage(data: SendMessageSchema) {
+    const { contacts, message, datetime } = data;
+    const date = datetime ? new Date(datetime) : undefined;
+    const hasScheduledDate = date && date > new Date();
+
+    const messageEntity = new Message();
+    messageEntity.text = message;
+    messageEntity.contacts = await Promise.all(
+      contacts.map((contact) =>
+        this.contactService.findOrCreateContacts(contact)
+      )
+    );
+
+    if (hasScheduledDate) {
+      messageEntity.status = Status.PENDING;
+      messageEntity.scheduledAt = date;
+      const result = await this.messageRepository.save(messageEntity);
+
+      this.scheduleMessage(result);
+      return result;
+    }
+
+    const sendResult = await this.whatsappService.sendMessage(
+      messageEntity.contacts.map((contact) => contact.chatId),
+      message
+    );
+    messageEntity.status = this.getMessageStatus(sendResult);
+
+    return this.messageRepository.save(messageEntity);
+  }
+
+  public getMessageStatus(result: PromiseSettledResult<WAWebJS.Message>[]) {
+    return result.every((r) => r.status === "fulfilled")
+      ? Status.SENT
+      : result.every((r) => r.status === "rejected")
+      ? Status.FAILED
+      : Status.PARTIAL;
+  }
+
+  public async updateMessageStatus(messageId: number, status: Status) {
+    return this.messageRepository.update(messageId, { status });
+  }
+
+  public scheduleMessage(message: Message): void {
+    schedule.scheduleJob(message.scheduledAt!, async () => {
+      try {
+        const sendResult = await this.whatsappService.sendMessage(
+          message.contacts.map((contact) => contact.chatId),
+          message.text
+        );
+        logger.log(
+          loggerLevels.info,
+          `Message sent to ${message.contacts
+            .map((item) => item.name)
+            .join(", ")}`
+        );
+
+        this.updateMessageStatus(message.id, this.getMessageStatus(sendResult));
+      } catch (error) {
+        logger.error(
+          `Error sending message to ${message.contacts
+            .map((item) => item.name)
+            .join(", ")}`,
+          error
+        );
+      }
+    });
+  }
+
+  private async syncPendingMessages() {
+    const pendingMessages = await this.messageRepository.find({
+      where: { status: Status.PENDING },
+    });
+
+    const currentDate = new Date();
+    pendingMessages
+      .filter((item) => item.scheduledAt && item.scheduledAt > currentDate)
+      .forEach((message) => this.scheduleMessage(message));
+  }
+}
